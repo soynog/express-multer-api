5f1ea4a771f074f00277601b73238b99cef4a231
soynog <nicolas.o.garcia@gmail.com>
2016-05-12 14:59:45 -0400

    Complete command line upload to AWS
    Generate low collision key for uploaded files
    Store in separate folders by date

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,7 +1,13 @@
 'use strict';
 
+require('dotenv').config();
+
 const fs = require('fs'); // Filesystem Module
 const fileType = require('file-type'); // FileType Determinant Module
+const AWS = require('aws-sdk'); // Amazon Web Services - Software Development Kit
+const crypto = require('crypto'); // Crypto module for generating random filenames
+
+const s3 = new AWS.S3();
 
 let filename = process.argv[2] || '';
 
@@ -21,24 +27,49 @@ const readFile = (filename) =>
   });
 });
 
+// determine filetype
 const mimeType = (data) =>
   Object.assign({
     ext: 'bin',
     mime: 'application/octet-stream'
   }, fileType(data));
 
-const awsUpload = (file) => {
-  const options = {
-    ACL: 'public-read', // Permissions on the uploaded file
-    Body: file.data, // The file itself to upload
-    Bucket: 'nog-wdi-upload-bucket', // My bucket on AWS
-    ContentType: file.mime, // Unencoded binary data
-    Key: `test/test.${file.ext}` // Where it's going in the bucket
-  };
-  return Promise.resolve(options);
-};
+// create a random string for filenames
+const randomHexString = (length) =>
+  new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (err, buf) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buf.toString('hex'));
+      }
+    });
+  });
 
-
+// Upload to Amazon Web Services
+const awsUpload = (file) =>
+  randomHexString(16)
+  .then((filename) => {
+    let dir = new Date().toISOString().split('T')[0];
+    return {
+      ACL: 'public-read', // Permissions on the uploaded file
+      Body: file.data, // The file itself to upload
+      Bucket: process.env.AWS_S3_BUCKET_NAME, // My bucket on AWS
+      ContentType: file.mime, // Unencoded binary data
+      Key: `${dir}/${filename}.${file.ext}` // Where it's going in the bucket
+    };
+  })
+  .then((params) =>
+    new Promise((resolve, reject) => {
+      s3.upload(params, (err, data) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    })
+  );
 
 // Read file and then...
 readFile(filename)
@@ -48,5 +79,5 @@ readFile(filename)
   return file;
 })
 .then(awsUpload)
-.then((file) => console.log(file))
+.then((s3response) => console.log(s3response))
 .catch((err) => console.error(err));
