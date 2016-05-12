d147e44f01bfc5f3240342b4b7a384db4bf315fa
soynog <nicolas.o.garcia@gmail.com>
2016-05-12 11:26:31 -0400

    Wrap readFile function in a promise
    Handle async in comprehensible way

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -5,11 +5,22 @@ const fs = require('fs'); // Filesystem Module
 let filename = process.argv[2] || '';
 
 // read file, then upload to Amazon S3
-// by not specifying an encoding (e.g. utf8), will get back a buffer (binary data)
-fs.readFile(filename, (err, data) => {
-  if (err) {
-    return console.error(err);
-  }
+const readFile = (filename) =>
 
-  console.log(`${filename} is ${data.length} bytes long.`);
+  // implicit return of new Promise (one-line function declaration)
+  new Promise((resolve, reject) => {
+
+  // by not specifying an encoding (e.g. utf8), will get back a buffer (binary data)
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
 });
+
+// Read file and then...
+readFile(filename)
+.then((data) => console.log(`${filename} is ${data.length} bytes long.`))
+.catch((err) => console.error(err));
