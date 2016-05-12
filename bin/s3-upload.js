48b6cc697db3b0241e5efc319a1842e335b84537
soynog <nicolas.o.garcia@gmail.com>
2016-05-12 15:29:25 -0400

    Separate out AWS Upload Function to its own file

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 
 const fs = require('fs'); // Filesystem Module
 const fileType = require('file-type'); // FileType Determinant Module
-const AWS = require('aws-sdk'); // Amazon Web Services - Software Development Kit
-const crypto = require('crypto'); // Crypto module for generating random filenames
-
-const s3 = new AWS.S3();
+const awsS3Upload = require('../lib/aws-s3-upload'); // Module for making AWS Uploads
 
 let filename = process.argv[2] || '';
 
@@ -27,49 +24,13 @@ const readFile = (filename) =>
   });
 });
 
+// always return object with extension and mime
 // determine filetype
 const mimeType = (data) =>
   Object.assign({
     ext: 'bin',
     mime: 'application/octet-stream'
   }, fileType(data));
-
-// create a random string for filenames
-const randomHexString = (length) =>
-  new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (err, buf) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buf.toString('hex'));
-      }
-    });
-  });
-
-// Upload to Amazon Web Services
-const awsUpload = (file) =>
-  randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-    return {
-      ACL: 'public-read', // Permissions on the uploaded file
-      Body: file.data, // The file itself to upload
-      Bucket: process.env.AWS_S3_BUCKET_NAME, // My bucket on AWS
-      ContentType: file.mime, // Unencoded binary data
-      Key: `${dir}/${filename}.${file.ext}` // Where it's going in the bucket
-    };
-  })
-  .then((params) =>
-    new Promise((resolve, reject) => {
-      s3.upload(params, (err, data) => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    })
-  );
 
 // Read file and then...
 readFile(filename)
@@ -78,6 +39,6 @@ readFile(filename)
   file.data = data;
   return file;
 })
-.then(awsUpload)
+.then(awsS3Upload)
 .then((s3response) => console.log(s3response))
 .catch((err) => console.error(err));
